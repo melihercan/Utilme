b1758f2cef0664ad8b9c55b3af91725b9caec762
Melih Ercan <melihercan@gmail.com>
2022-02-25 21:06:37 +0300

    Updated SdpTransport from WebRTCme.

---
+++ SdpTransform/Enums/RTCIceProtocol.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Utilme.SdpTransform
+{
+    [JsonConverter(typeof(JsonCamelCaseStringEnumConverter))]
+    public enum RTCIceProtocol
+    {
+        Udp,
+        Tcp
+    }
+}
